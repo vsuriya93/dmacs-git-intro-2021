0f301e85da06f6a200e2f58213cc298f9d72c6d3
Sai Suraj <53555104+firehawk23@users.noreply.github.com>
2021-10-09 12:10:08 +0530

    Update

--- dmacs_utils/G1_LinkedLists.py
+++ dmacs_utils/G1_LinkedLists.py
@@ -1,2 +1,19 @@
-class LinkedList(object):
-    pass
+class LLNode:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self,data):
+        node = LLNode(data)
+        if self.head is None:
+            self.head = node
+            return
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = node
+        return
